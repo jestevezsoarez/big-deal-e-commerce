a4e367ec4e77c8777abbb15e928f1ce55cc23249
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-05 05:11:33 -0300

    installation of react-router-dom and link routes with pages

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,11 +1,30 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import Home from '../Home/home';
+import MyAccount from '../MyAccount/myAccount';
+import MyOrder from '../MyOrder/myOrder';
+import MyOrders from '../MyOrders/myOrders';
+import NotFound from '../NotFound/notFound';
+import SignIn from '../SignIn/signIn';
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+    let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
 
+  return routes;
+}
+
+function App() {
   return (
-    <div className='bg-red-100'>
-      Big Deal E-Commerce
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
