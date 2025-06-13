322cd88d6f7afa5293c243a6e10d2479a2b17d2f
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-13 19:35:46 -0300

    Add hover to add to shopping cart buttons

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -16,7 +16,7 @@ const Card = ({ item }) => {
           alt={item.title}
         />
         <div
-          className="absolute top-0 right-0 flex justify-center items-center bg-blue-100 w-6 h-6 rounded-full m-2 p-1"
+          className="absolute top-0 right-0 flex justify-center items-center bg-amber-400 hover:bg-amber-500 w-6 h-6 rounded-full m-2 p-1"
           onClick={() => context.setCount(context.count + 1)}
         >
           <svg
@@ -35,7 +35,13 @@ const Card = ({ item }) => {
           </svg>
         </div>
       </figure>
-      <button className="bg-amber-400 text-blue-50 w-full">Add to Cart</button>
+      <button className="bg-amber-400 hover:bg-amber-500 text-white w-full font-medium">Add to Cart</button>
+      {/* <button
+        type="button"
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 w-full font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      >
+        Add to Cart
+      </button> */}
       <p className="flex justify-between">
         <span className="text-sm font-light">{item.title}</span>
         <span className="text-lg font-bold">${item.price}</span>
