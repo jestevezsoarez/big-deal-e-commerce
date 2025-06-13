d2a706ba15dfec8ae5974180765037f8ae04e50f
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-13 19:45:10 -0300

    change title to fit in card, align title with price using flex

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,7 +5,7 @@ const Card = ({ item }) => {
   const context = useContext(ShoppingCartContext);
 
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg mb-5">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {item.category?.name}
@@ -35,15 +35,9 @@ const Card = ({ item }) => {
           </svg>
         </div>
       </figure>
-      <button className="bg-amber-400 hover:bg-amber-500 text-white w-full font-medium">Add to Cart</button>
-      {/* <button
-        type="button"
-        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 w-full font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-      >
-        Add to Cart
-      </button> */}
-      <p className="flex justify-between">
-        <span className="text-sm font-light">{item.title}</span>
+      <button className="bg-amber-400 hover:bg-amber-500 text-white w-full font-medium rounded-md">Add to Cart</button>
+      <p className="flex justify-between items-center">
+        <span className="text-sm font-light">{item.title.slice(0, 20)}</span>
         <span className="text-lg font-bold">${item.price}</span>
       </p>
     </div>
