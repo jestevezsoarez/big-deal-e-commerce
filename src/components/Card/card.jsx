608a08de1ee1bca454a167f72597f06f2570be5d
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-14 01:21:41 -0300

    minor changes related to add to cart icon

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,7 +5,7 @@ const Card = ({ item }) => {
   const context = useContext(ShoppingCartContext);
 
   return (
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg mb-5">
+    <div className="bg-white w-56 h-60 rounded-lg mb-5">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {item.category?.name}
@@ -15,27 +15,13 @@ const Card = ({ item }) => {
           src={item.image}
           alt={item.title}
         />
-        <div
-          className="absolute top-0 right-0 flex justify-center items-center bg-amber-400 hover:bg-amber-500 w-6 h-6 rounded-full m-2 p-1"
-          onClick={() => context.setCount(context.count + 1)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-        </div>
       </figure>
-      <button className="bg-amber-400 hover:bg-amber-500 text-white w-full font-medium rounded-md">Add to Cart</button>
+      <button 
+        className="bg-blue-400 hover:bg-blue-500 text-white w-full font-medium rounded-md cursor-pointer"
+        onClick={() => context.setCount(context.count + 1)}  
+      >        
+          Add to Cart
+      </button>
       <p className="flex justify-between items-center">
         <span className="text-sm font-light">{item.title.slice(0, 20)}</span>
         <span className="text-lg font-bold">${item.price}</span>
