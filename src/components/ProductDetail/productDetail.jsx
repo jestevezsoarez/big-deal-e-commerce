1f7b8e85298da13420e676b17f7381c87184cef6
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-18 03:05:40 -0300

    showing details in product detail panel

--- src/components/ProductDetail/productDetail.jsx
+++ src/components/ProductDetail/productDetail.jsx
@@ -10,12 +10,20 @@ const ProductDetail = () => {
     return (
       <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
         <div className="flex justify-between items-center p-6">
-          <h2 className="font-medium text-xl">Detail</h2>
+          <h2 className="font-medium text-xl">Details</h2>
           <XMarkIcon
             className="size-6 text-black cursor-pointer"
             onClick={() => context.closeProductDetail()}
           />
         </div>
+        <figure className="px-6">
+          <img className="w-full h-full rounded-lg object-cover" src={context.produtToShow.image} alt={context.produtToShow.title} />
+        </figure>
+        <p className="flex flex-col p-6">
+          <span className="font-medium text-2xl mb-2">{context.produtToShow.price}</span>
+          <span className="font-medium text-md">{context.produtToShow.title}</span>
+          <span className="font-light text-sm">{context.produtToShow.description}</span>
+        </p>
       </aside>
     );
   }
