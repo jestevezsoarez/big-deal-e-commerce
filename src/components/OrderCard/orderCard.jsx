197afdc46618d5c128b141768f52d17a27cc74b0
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-22 21:36:01 -0300

    working in the design of the order card

--- src/components/OrderCard/orderCard.jsx
+++ src/components/OrderCard/orderCard.jsx
@@ -2,16 +2,20 @@ import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const OrderCard = ({item}) => {
     return (
-        <div>
-            <figure>
-                <img src={item.image} alt={item.title} />
+        <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+            <figure className="w-20 h-20">
+                <img className="w-full h-full rounded-lg object-cover" src={item.image} alt={item.title} />
             </figure>
-            <span>{item.title}</span>
-            <span>{item.price}</span>
-            <XMarkIcon
-                className="size-6 text-black cursor-pointer"
-                onClick={() => context.closeCheckoutSideMenu()}
-            />
+            <p className="text-sm font-light">{item.title}</p>
+            </div>
+            <div className="flex items-center gap-2">
+                <p className="text-lg font-medium">{item.price}</p>
+                <XMarkIcon
+                    className="size-6 text-black cursor-pointer"
+                    onClick={() => context.closeCheckoutSideMenu()}
+                />
+            </div>
         </div>
     )
 }
