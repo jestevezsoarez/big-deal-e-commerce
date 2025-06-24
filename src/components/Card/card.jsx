a6e1c3efa8fd69d601e3b31e110f595c6d71baab
Javier Estevez <javierdaniloestevez@gmail.com>
2025-06-24 19:36:00 -0300

    change styles in card related to category and typo correction

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -49,8 +49,8 @@ const Card = ({ item }) => {
       onClick={() => showProductDetail(item)}
     >
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          {item.category?.name}
+        <span className="absolute bottom-0 left-0 bg-amber-300 rounded-lg text-black text-xs m-2 px-3 py-0.5">
+          {item.category}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
