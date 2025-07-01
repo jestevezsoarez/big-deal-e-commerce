a880b8178a93b6edd8e066306b24294a57190613
Javier Estevez <javierdaniloestevez@gmail.com>
2025-07-01 04:27:13 -0300

    working in my orders page to view all orders made

--- src/Pages/MyOrders/myOrders.jsx
+++ src/Pages/MyOrders/myOrders.jsx
@@ -1,9 +1,25 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context/context";
 import Layout from "../../components/Layout/layout";
+import OrderCards from "../../components/OrderCards/orderCards";
 
 function MyOrders() {
+    const context = useContext(ShoppingCartContext);
+
     return (
         <Layout>
-            MyOrders            
+            MyOrders
+            {
+                context.order.map((order, index) => (
+                    <Link key={index} to={`/my-orders/${order.id}`}>
+                        <OrderCards
+                            totalProducts={order.totalPrice}
+                            totalPrice={order.totalProducts}
+                        />                    
+                    </Link>
+                ))
+            }                       
         </Layout>
     )
 }
