c054b6c2dc70539caf7c66f3b7ca0b6b92528ec0
Javier Estevez <javierdaniloestevez@gmail.com>
2025-07-05 19:18:37 -0300

    working in orders styles using tailwind css

--- src/components/OrderCards/orderCards.jsx
+++ src/components/OrderCards/orderCards.jsx
@@ -1,16 +1,18 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+
 
 const OrderCards = props => {
     const { totalProducts, totalPrice } = props;
 
     return (
-        <div className="flex justify-between items-center mb-3 border border-black w-60 rounded-lg font-light p-3">
-            <div className="flex flex-col">
+        <div className="flex justify-between items-center mb-3 border border-black w-80 rounded-lg font-light p-4">
+            <div className="flex flex-col justify-between">
                 <span>01.07.2025</span>
                 <span>{totalProducts} articles</span>                
             </div>
             <div>
-                <span>{totalPrice}</span>
+                <span className="font-medium text-2xl">${totalPrice}</span>
+                <ChevronRightIcon className="w-6 h-6 text-black cursor-pointer"></ChevronRightIcon>
             </div>
         </div>
     )
