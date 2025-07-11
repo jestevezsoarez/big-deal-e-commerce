6204b306bb6d8f42bee2176888bc197357b54c52
Javier Estevez <javierdaniloestevez@gmail.com>
2025-07-11 04:17:53 -0300

    clear input to search products after the user purchase products

--- src/components/CheckoutSideMenu/checkOutSideMenu.jsx
+++ src/components/CheckoutSideMenu/checkOutSideMenu.jsx
@@ -26,6 +26,7 @@ const CheckOutSideMenu = () => {
 
     context.setOrder([...context.order, orderToAdd]);
     context.setCartProducts([]);
+    context.setSearchByTitle(null);
   };
 
   const getOrderDate = () => {
